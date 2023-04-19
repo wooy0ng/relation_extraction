ffac5c947b7133c2c18c8d532ce1118c689e4f10
wooy0ng <yygg9800@naver.com>
2023-04-19 16:08:35 +0900

    [feat]: add wandb logging

--- code/train.py
+++ code/train.py
@@ -1,10 +1,12 @@
 import torch
+import wandb
 from transformers import (
     AutoConfig, 
     AutoModelForSequenceClassification,
     TrainingArguments,
     Trainer
 )
+from datetime import datetime, timezone, timedelta
 from data.dataset import RelationExtractionDataset
 from model.metrics import compute_metrics
 
@@ -12,12 +14,27 @@ from model.metrics import compute_metrics
 TODO: refactoring
     TODO: make compute_metrics() function
     TODO: do training
-        TODO: studying huggingface architecture
-        TODO: using wandb
+        TODO: studying huggingface architecture (o)
+        TODO: using wandb (o)
 '''
 
 
+
+
 def train() -> None:
+    def wandb_init(project="relation extraction", name="name"):
+        with open('.wandb_key', 'r+') as f:
+            key = f.read()
+        
+        try:
+            wandb.login(key=key)
+        except:
+            anony = "must"
+            print('If you want to use your W&B account, go to Add-ons -> Secrets and provide your W&B access token. Use the Label name as wandb_api. \nGet your W&B access token from here: https://wandb.ai/authorize')
+        
+        time_serial = datetime.now(timezone(timedelta(hours=9))).strftime("%y%m%d-%h%M%S")
+        wandb.init(project=project, name=f"{name}_{time_serial}")
+        
     # load from transformers
     model_name = "klue/bert-base"
     
@@ -32,23 +49,26 @@ def train() -> None:
         model_name, config=model_config
     ).to(device)
     
+    # wandb init
+    wandb_init(name="run1")
+    
     # init training arguments
     training_args = TrainingArguments(
         output_dir='./results',
-        save_total_limit=3,
-        save_steps=1000,
-        num_train_epochs=3,
         learning_rate=5e-5,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
+        num_train_epochs=3,
+        
         warmup_steps=1000,
-        weight_decay=0.01,
-        logging_dir='./logs',
-        logging_steps=100,
         evaluation_strategy='epoch',
         save_strategy='epoch',
+        save_total_limit=3,
+        logging_dir='./logs',
+        logging_steps=50,
         load_best_model_at_end=True,
         fp16=True,
+        report_to='wandb'
     )
     
     trainer = Trainer(
